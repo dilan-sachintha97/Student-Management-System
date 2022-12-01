0b17e5172f45c7aa61d32326906b9036c430565e
dilan-sachintha97 <dil97an@gmail.com>
2022-12-01 21:26:31 +0530

    init project

---
+++ src/controller/StudentFormController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class StudentFormController {
+
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public ToggleGroup Gender;
+    public RadioButton rbtnFemale;
+    public TableView tblStudentDetails;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colGender;
+    public TableColumn colOption;
+
+    public void btnOnActionSave(ActionEvent actionEvent) {
+    }
+
+    public void btnOnActionUpdate(ActionEvent actionEvent) {
+    }
+}
